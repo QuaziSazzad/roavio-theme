de5059fa1b36a80363e6b2716a27b258cfbf63d0
Quazi Sazzad <qsazzad@gmail.com>
2025-09-28 12:15:00 +0600

    feat: updated

--- 404.php
+++ 404.php
@@ -12,35 +12,35 @@ use RoavioTheme\Classes\Roavio_Helper as Helper;
 
 get_header();
 
-$error_title    = Helper::get_option('error_title', esc_html__('"Oops! It looks like something went wrong."', 'roavio'));
-$error_message  = Helper::get_option('error_bottom_message', esc_html__('The page you are looking for does not exist or has been moved', 'roavio'));
+$not_found_text    = Helper::get_option('not_found_text', esc_html__('404 (page not found)', 'roavio'));
+$error_title    = Helper::get_option('error_title', esc_html__('Sorry! page can’t be found', 'roavio'));
+$error_message  = Helper::get_option('error_bottom_message', esc_html__('Sorry, the page you’re looking for doesn’t exist or may have been moved.', 'roavio'));
 $button_text    = Helper::get_option('error_button_text', esc_html__('Go to Home', 'roavio'));
 $error_page_image = Helper::get_option('error_page_image', ['url' => ROAVIO_ASSETS . '/img/error-404.png']);
 
 ?>
-<!-- 404 Error Area Start -->
-<section class="404-error-area py-130 rpy-100 rel z-1">
+
+<!-- error Section Start -->
+<section class="error-section section-padding fix">
+    <div class="text"><?php echo esc_html($not_found_text); ?></div>
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-xl-9">
-                <div class="error-content text-center">
+            <div class="col-lg-6">
+                <div class="error-item">
                     <?php if (!empty($error_page_image['url'])): ?>
-                        <img src="<?php echo esc_url($error_page_image['url']); ?>" alt="<?php esc_attr_e('404 Error', 'roavio'); ?>">
+                        <div class="error-image">
+                            <img src="<?php echo esc_url($error_page_image['url']); ?>" alt="<?php esc_attr_e('404 Error', 'roavio'); ?>">
+                        </div>
                     <?php endif; ?>
-                    <?php if ($error_title): ?>
+                    <div class="error-content">
                         <h2><?php echo esc_html($error_title); ?></h2>
-                    <?php endif; ?>
-                    <?php if ($error_message): ?>
                         <p><?php echo esc_html($error_message); ?></p>
-                    <?php endif; ?>
-
-                    <a href="<?php echo esc_url(home_url('/')); ?>" class="theme-btn mt-15"><?php echo esc_html($button_text); ?></a>
-
+                        <a href="<?php echo esc_url(home_url('/')); ?>" class="theme-btn" data-animation="fadeInUp" data-delay="1.3s"><?php echo esc_html($button_text); ?></a>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </section>
-<!-- 404 Error Area End -->
 <?php
 get_footer();
